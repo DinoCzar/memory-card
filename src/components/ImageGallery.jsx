eea216f55f00598fcbc901402061b633b5ea50ff
DinoCzar <ryanb_booking@yahoo.com>
2023-11-14 00:42:03 -0800

    Images rearrange after every click, player is alerted if win or lose

--- src/components/ImageGallery.jsx
+++ src/components/ImageGallery.jsx
@@ -2,73 +2,90 @@ import React, { useState, useEffect } from 'react';
 import GenerateImage from './GenerateImage';
 
 function ImageGallery() {
-  const initialTermsArray = [
-    'Shark',
-    'King',
-    'Butterfly',
-    'Robot',
-    'Puzzle',
-    'Planet',
-    'Dinosaur',
-    'Sword',
-    'Pyramids',
-  ];
+	const initialTermsArray = [
+		'Shark',
+		'King',
+		'Butterfly',
+		'Robot',
+		'Puzzle',
+		'Planet',
+		'Dinosaur',
+		'Sword',
+		'Pyramids',
+	];
 
-  const [clickCount, setClickCount] = useState(0);
-  const [termClickCounts, setTermClickCounts] = useState({});
-  const [termsArray, setTermsArray] = useState(initialTermsArray);
+	const [clickCount, setClickCount] = useState(0);
+	const [termClickCounts, setTermClickCounts] = useState({});
+	const [termsArray, setTermsArray] = useState(initialTermsArray);
 
-  const handleClick = (term) => {
-    // Update the total click count
-    setClickCount((prevCount) => prevCount + 1);
+	const handleClick = (term) => {
+		// Update the total click count
+		setClickCount((prevCount) => prevCount + 1);
 
-    // Update the click count for the clicked term
-    setTermClickCounts((prevClickCounts) => ({
-      ...prevClickCounts,
-      [term]: (prevClickCounts[term] || 0) + 1,
-    }));
-  };
+		// Update the click count for the clicked term
+		setTermClickCounts((prevClickCounts) => ({
+			...prevClickCounts,
+			[term]: (prevClickCounts[term] || 0) + 1,
+		}));
 
-  const rearrangePictures = () => {
-    // Shuffle the terms array randomly
-    const shuffledTerms = [...termsArray].sort(() => Math.random() - 0.5);
+		// Rearrange pictures randomly
+		rearrangePictures();
+	};
 
-    // Update the terms array state with the shuffled order
-    setTermsArray(shuffledTerms);
+	const rearrangePictures = () => {
+		// Shuffle the terms array randomly
+		const shuffledTerms = [...termsArray].sort(() => Math.random() - 0.5);
 
-    // Reset all click counts to 0
-    setTermClickCounts({});
-    
-    // Reset the total click count to 0
-    setClickCount(0);
-  };
+		// Update the terms array state with the shuffled order
+		setTermsArray(shuffledTerms);
+	};
 
-  useEffect(() => {
-    // Check if the click count for any term is 2
-    const termWithTwoClicks = Object.keys(termClickCounts).find(
-      (term) => termClickCounts[term] === 2
-    );
+	useEffect(() => {
+		// Check if the click count for any term is 2
+		const termWithTwoClicks = Object.keys(termClickCounts).find(
+			(term) => termClickCounts[term] === 2
+		);
 
-    if (termWithTwoClicks) {
-      // Execute rearrangePictures if the click count for the term is 2
-      rearrangePictures();
-    }
-  }, [termClickCounts]);
+		// Execute rearrangePictures if the click count for the term is 2
+		if (termWithTwoClicks) {
+			alert('You Lose!');
+			// Reset all click counts to 0
+			setTermClickCounts({});
 
-  return (
-    <div className='container'>
-      <div className='counter'>Score: {clickCount}</div>
-      <div className='gallery'>
-        {termsArray.map((term) => (
-          <div key={term} onClick={() => handleClick(term)}>
-            <p>{term}</p>
-            <GenerateImage term={term} />
-            <div>Click Count: {termClickCounts[term] || 0}</div>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+			// Reset the total click count to 0
+			setClickCount(0);
+
+			// Rearrange pictures randomly
+			rearrangePictures();
+		}
+
+		// Check if the total click count is 9
+		if (clickCount === 9) {
+			alert('You Win!');
+			// Reset all click counts to 0
+			setTermClickCounts({});
+
+			// Reset the total click count to 0
+			setClickCount(0);
+
+			// Rearrange pictures randomly
+			rearrangePictures();
+		}
+	}, [termClickCounts, clickCount]);
+
+	return (
+		<div className='container'>
+			<div className='counter'>Score: {clickCount}</div>
+			<div className='gallery'>
+				{termsArray.map((term) => (
+					<div key={term} onClick={() => handleClick(term)}>
+						<p>{term}</p>
+						<GenerateImage term={term} />
+					</div>
+				))}
+			</div>
+		</div>
+	);
 }
 
 export default ImageGallery;
