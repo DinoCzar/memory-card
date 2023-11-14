34ef018b0a690dd52fd452d9063d6e64fa3c94a6
DinoCzar <ryanb_booking@yahoo.com>
2023-11-14 00:03:11 -0800

    Update page layout

--- src/components/ImageGallery.jsx
+++ src/components/ImageGallery.jsx
@@ -1,13 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import GenerateImage from './GenerateImage';
+
 function ImageGallery() {
-	const termsArray = ['shark', 'king', 'butterfly', 'robot', 'puzzle', 'planet', 'dinosaur', 'sword', 'pyramids'];
+	const termsArray = [
+		'Shark',
+		'King',
+		'Butterfly',
+		'Robot',
+		'Puzzle',
+		'Planet',
+		'Dinosaur',
+		'Sword',
+		'Pyramids',
+	];
+
+	const [clickCount, setClickCount] = useState(0);
+
+	const handleClick = () => {
+		setClickCount((prevCount) => prevCount + 1);
+	};
 
 	return (
-		<div className='gallery'>
-			{termsArray.map((term, index) => (
-				<GenerateImage key={index} term={term} />
-			))}
+		<div className='container'>
+			<div className='counter'>Score: {clickCount}</div>
+			<div className='gallery'>
+				{termsArray.map((term) => (
+					<div key={termsArray.term} onClick={handleClick}>
+						<p>{term}</p>
+						<GenerateImage term={term} />
+					</div>
+				))}
+			</div>
 		</div>
 	);
 }
