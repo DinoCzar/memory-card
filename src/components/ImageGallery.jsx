2312f8fa4c3084d70fab421d9a1df4800f6a1db7
DinoCzar <ryanb_booking@yahoo.com>
2023-11-14 00:13:10 -0800

    Add click counter for each image

--- src/components/ImageGallery.jsx
+++ src/components/ImageGallery.jsx
@@ -2,37 +2,47 @@ import React, { useState } from 'react';
 import GenerateImage from './GenerateImage';
 
 function ImageGallery() {
-	const termsArray = [
-		'Shark',
-		'King',
-		'Butterfly',
-		'Robot',
-		'Puzzle',
-		'Planet',
-		'Dinosaur',
-		'Sword',
-		'Pyramids',
-	];
+  const termsArray = [
+    'Shark',
+    'King',
+    'Butterfly',
+    'Robot',
+    'Puzzle',
+    'Planet',
+    'Dinosaur',
+    'Sword',
+    'Pyramids',
+  ];
 
-	const [clickCount, setClickCount] = useState(0);
+  const [clickCount, setClickCount] = useState(0);
+  const [termClickCounts, setTermClickCounts] = useState({});
 
-	const handleClick = () => {
-		setClickCount((prevCount) => prevCount + 1);
-	};
+  const handleClick = (term) => {
+    // Update the total click count
+    setClickCount((prevCount) => prevCount + 1);
 
-	return (
-		<div className='container'>
-			<div className='counter'>Score: {clickCount}</div>
-			<div className='gallery'>
-				{termsArray.map((term) => (
-					<div key={termsArray.term} onClick={handleClick}>
-						<p>{term}</p>
-						<GenerateImage term={term} />
-					</div>
-				))}
-			</div>
-		</div>
-	);
+    // Update the click count for the clicked term
+    setTermClickCounts((prevClickCounts) => ({
+      ...prevClickCounts,
+      [term]: (prevClickCounts[term] || 0) + 1,
+    }));
+  };
+
+  return (
+    <div className='container'>
+      <div className='counter'>Score: {clickCount}</div>
+      <div className='gallery'>
+        {termsArray.map((term) => (
+          <div key={term} onClick={() => handleClick(term)}>
+            <p>{term}</p>
+            <GenerateImage term={term} />
+            <div>Click Count: {termClickCounts[term] || 0}</div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default ImageGallery;
+
